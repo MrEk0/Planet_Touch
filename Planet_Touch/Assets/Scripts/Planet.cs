0ad46ea29e725341daf9bd6ff2da7fdaa103a7a3
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-13 14:12:59 +0300

    planet creation

---
+++ Planet_Touch/Assets/Scripts/Planet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Planet", menuName ="Planet")]
+public class Planet : ScriptableObject
+{
+    [SerializeField] float gravitySpeed;
+    [SerializeField] Color skyColor;
+
+}

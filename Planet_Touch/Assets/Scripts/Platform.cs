10302666208a067c60ffaa23ece647ab01faebd1
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2019-08-13 14:11:46 +0300

    platform behaviour

---
+++ Planet_Touch/Assets/Scripts/Platform.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.name=="Ball")
+        {
+            spriteRenderer.color = RandomColor();
+        }
+    }
+
+    private Color RandomColor()
+    {
+        float green = Random.Range(0, 255);
+        float blue = Random.Range(0, 255);
+        float red = Random.Range(0, 255);
+
+        Color color = new Color(red / 255, green / 255, blue / 255);
+
+        return color;
+    }
+}
